62db685571956822a545e226b823166ec2223bf6
dldydgns530@gmail.com <dldydgns530@gmail.com>
2021-11-30 14:40:14 +0900

    activity chat2

--- app/src/main/java/com/example/chatproject/ChatActivity.kt
+++ app/src/main/java/com/example/chatproject/ChatActivity.kt
@@ -6,26 +6,59 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var charRecyclerView: RecyclerView
     private lateinit var messageBox: EditText
     private lateinit var sendButton: ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    var receiverRoom: String? = null
+    var senderRoom: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val intent = Intent()
+        // val intent = Intent()
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
 
         charRecyclerView = findViewById(R.id.charRecyclerView)
         messageBox = findViewById(R.id.messageBox)
         sendButton = findViewById(R.id.sentButton)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
 
+        // logic for adding data to recyclerView
+
+
+        // adding the message to database
+        sendButton.setOnClickListener{
+
+            val message = messageBox.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+            messageBox.setText("")
+        }
     }
 }
--- app/src/main/java/com/example/chatproject/UserAdapter.kt
+++ app/src/main/java/com/example/chatproject/UserAdapter.kt
@@ -28,7 +28,7 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>):
             val intent = Intent(context,ChatActivity::class.java)
 
             intent.putExtra("name",currentUser.name)
-            intent.putExtra("uid",FirebaseAuth.getInstance().currentUser?.uid)
+            intent.putExtra("uid",currentUser.uid)
 
             context.startActivity(intent)
         }
